410bd12e8ca8814f6fc5266b8b7c94de58491263
root <zethukhumalo302@gmail.com>
2023-02-23 14:30:42 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,25 @@ i = 1;
 while (i <= 100)
 {
 if (i % 3 == 0 && i % 5 == 0)
+{
 _putchar("FizzBuzz");
+}
 else if (i % 3 == 0)
+{
 _putchar("Fizz");
+}
 else if (i % 5 == 0)
+{
 _putchar("Buzz");
+}
 else
+{
 _putchar("%d", i);
+}
 if (i != 100)
+{
 _putchar(" ");
-i++;
+}
 }
 _putchar("\n");
 return (0);
